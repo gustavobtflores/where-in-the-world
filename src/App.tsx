21232fafe5249f091cd9a17843cc014f94231334
Gustavo Bernardo <gustavobtflores@gmail.com>
2022-06-21 20:46:14 -0300

    select html and some adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ interface CountryResponse {
 
 function App() {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [regions, setRegions] = useState<string[]>([]);
   const [countrySearch, setCountrySearch] = useState("");
   const filteredCountries = countrySearch ? countries.filter((country) => country.name.toLowerCase().includes(countrySearch.toLowerCase())) : countries;
 
@@ -45,23 +46,54 @@ function App() {
     getCountries();
   }, []);
 
+  useEffect(() => {
+    const getRegions = () => {
+      var uniqueRegions: string[] = [];
+
+      countries.forEach((country) => {
+        if (uniqueRegions.includes(country.region)) return;
+        uniqueRegions.push(country.region);
+      });
+
+      setRegions(uniqueRegions);
+    };
+
+    getRegions();
+  }, [countries]);
+
   const handleCountrySearch = (event: FormEvent<HTMLInputElement>) => {
     setCountrySearch(event.currentTarget.value);
+  };
+
+  const handleCountryPageOpen = (countryName: string) => {
+    const countryId = countryName.toLowerCase().replaceAll(" ", "");
+    console.log(countryId);
   };
 
   return (
     <>
       <Header />
       <Container>
-        <input
-          placeholder="Search for a country..."
-          type="text"
-          className="font-[Nunito Sans] w-96 py-4 pl-8 shadow-md mt-8 bg-light-white rounded-lg outline-none focus:placeholder:invisible focus:border-light-dark-blue border-solid border-[1px] transition-colors"
-          onChange={handleCountrySearch}
-        />
+        <header className="flex justify-between items-center mt-8">
+          <input
+            placeholder="Search for a country..."
+            type="text"
+            className="font-[Nunito Sans] max-w-xs w-full py-4 pl-8 shadow-md bg-light-white rounded-lg outline-none focus:placeholder:invisible focus:border-light-dark-blue border-solid border-[1px] transition-colors"
+            onChange={handleCountrySearch}
+          />
+          <select name="" id="">
+            {regions.map((region) => {
+              return (
+                <option value={region.toLowerCase()} key={region}>
+                  {region}
+                </option>
+              );
+            })}
+          </select>
+        </header>
         <div className="grid grid-cols-countryItem gap-[3.75rem] mt-4">
           {filteredCountries.map((country) => {
-            return <CountryItem key={country.name} country={country} onClick={() => {}} />;
+            return <CountryItem key={country.name} country={country} onClick={handleCountryPageOpen} />;
           })}
         </div>
       </Container>
